d9528e79ca867daebd28a1cf43da852031f68561
Jamie <63889064+jamjoung@users.noreply.github.com>
2023-02-19 16:11:59 -0700

    Initial Upload

---
+++ PostoryApp/src/components/LocationPin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import googleMapReact from 'google-map-react'
+
+// const LocationPin = ({ text }) => (
+//     <div className="pin">
+//       <Icon icon={locationIcon} className="pin-icon" />
+//       <p className="pin-text">{text}</p>
+//     </div>
+//   )
